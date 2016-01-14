4b35b1257da378dfeafc1db611d4fa8591b05fcd
Troy Leach <troyleach29@gmail.com>
2016-01-13 21:01:58 -0600

    initial commit, page loads and scss is working

---
+++ Gulpfile.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+require('require-dir')('./gulp');
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('build');
+});
