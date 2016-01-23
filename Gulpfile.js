221f55f6112a248fc9a27b47cb636b196713a030
Troy Leach <troyleach29@gmail.com>
2016-01-23 12:01:13 -0600

    another way of deploying

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,14 @@ var sass = require('gulp-ruby-sass');
 
 require('require-dir')('./gulp');
 
+gulp.task('serveprod', function() {
+  connect.server({
+    root: [your_project_path],
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
+});
+
 gulp.task('default', ['clean'], function() {
     gulp.start('build');
 });
