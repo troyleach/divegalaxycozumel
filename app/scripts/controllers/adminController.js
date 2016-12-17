939a2cd956c290550b34ea0937a4ce5d6019d42c
Troy Leach <troyleach29@gmail.com>
2016-12-17 10:08:11 -0600

    finished the pricing edit button

--- app/scripts/controllers/adminController.js
+++ app/scripts/controllers/adminController.js
@@ -9,11 +9,12 @@
     var admin = this;
     this.pageIdentifier = "Admin Dashboard";
     this.panelTitle = 'Adminstration Things';
-        this.panelTitle = 'Single Day Rates';
-        admin.singleDay = 'partials/edit_pricing_partials/single_day.html';
-        admin.rentalGear = 'partials/edit_pricing_partials/rental_gear.html';
-        admin.training = 'partials/edit_pricing_partials/training.html';
-        admin.specialties = 'partials/edit_pricing_partials/specialties.html';
+    this.panelTitle = 'Single Day Rates';
+    admin.singleDay = 'partials/edit_pricing_partials/single_day.html';
+    admin.rentalGear = 'partials/edit_pricing_partials/rental_gear.html';
+    admin.training = 'partials/edit_pricing_partials/training.html';
+    admin.specialties = 'partials/edit_pricing_partials/specialties.html';
+    admin.miscellaneous = 'partials/edit_pricing_partials/miscellaneous_pricings.html';
 
         // $scope.model = {
         //     name: 'tabs'
@@ -50,18 +51,13 @@
                 var controllers = {SpecialtiesCtrl: 'specialty',
                                    TrainingsCtrl: 'training',
                                    DivingsCtrl: 'diving',
+                                   MiscellaneousCtrl: 'miscellaneous_pricing',
                                    RentalsCtrl: 'rental'};
                 var requiredParams = controllers[$scope.category];
                 data['id'] = selectedItem.id;
                 data[requiredParams] = {title: selectedItem.title,
                                         price: selectedItem.price,
                                         description: selectedItem.description};
-
-                // var data = {id: selectedItem.id,
-                //             specialty: {title: selectedItem.title,
-                //                        price: selectedItem.price,
-                //                       description: selectedItem.description}};
-                // $scope.selected = selectedItem;
 
                 // TODO I would really like to make this dynamic UPDATE these posts requests should happen in the modal controller and only exit the modal when the resonse is success.
                 // TODO move the below actions into the modal controllers only close if respnose is success
@@ -73,6 +69,8 @@
                     updatePricingFactory.updateRentalsPricing(data);
                 } else if($scope.category == 'DivingsCtrl') {
                     updatePricingFactory.updateDivingsPricing(data);
+                } else if($scope.category == 'MiscellaneousCtrl') {
+                    updatePricingFactory.updateMiscellaneousPricing(data);
                 }
                 // TODO not sure the above is the right place to do this, but It did make it over to the service updatePricing Service, see notes there - this works accept the callback doesn't work
             }, function () {
@@ -96,8 +94,8 @@
         });
 
         getPricingFactory.getMiscellaneousPricing().then(function(response) {
-            var currentPricingMiscellaneous = response;
-            admin.parkFee = currentPricingMiscellaneous[0].price;
+            admin.currentPricingMiscellaneous = response;
+            // admin.parkFee = currentPricingMiscellaneous[0].price;
         });
   }]);
 })();
